7f77cb2aacc5d3ce78a9166bf26c9c14be1f461c
Daniel <danielalvesmajor@outlook.com>
2020-05-12 14:10:02 -0300

    Exibindo erros no input

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -43,6 +43,8 @@ const Input: React.FC<InputProps> = ({name, icon: Icon, ...rest}) => {
             defaultValue={defaultValue} 
             ref={inputRef} 
             {...rest} />
+
+            {error}
         </Container>
     );
 };
--- src/pages/Signup/index.tsx
+++ src/pages/Signup/index.tsx
@@ -1,7 +1,10 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useRef } from 'react';
 import { Form } from '@unform/web';
+import { FormHandles } from '@unform/core';
+
 import { FiArrowLeft, FiMail, FiLock, FiUser } from 'react-icons/fi'
 import * as Yup from 'yup';
+import getValidationErrors from '../../utils/getValidationErrors';
 
 import { Container, Background, Content } from './styles';
 
@@ -11,8 +14,12 @@ import Input from '../../components/Input';
 import Button from '../../components/Button';
 
 const Signup: React.FC = () =>  {
+    const formRef = useRef<FormHandles>(null);
+
     const handleSubmit = useCallback( async (data: object) => {
         try {
+            formRef.current?.setErrors({});
+
             const schema = Yup.object().shape({
                 name: Yup.string().required('Nome obrigatório'),
                 email: Yup.string().required('E-mail obrigatório').email('Digite um e-mail válido'),
@@ -22,9 +29,11 @@ const Signup: React.FC = () =>  {
             await schema.validate(data, {
                 abortEarly: false,
             });
-            
+
         } catch (err) {
-            console.log(err);
+            const errors = getValidationErrors(err);
+
+            formRef.current?.setErrors(errors);
         }
     }, []); 
       
@@ -37,7 +46,7 @@ const Signup: React.FC = () =>  {
         <Content>
             <img src={logoImg} alt="GoBarber" />
 
-            <Form initialData={{ name: 'Daniel' }} onSubmit={handleSubmit}>
+            <Form ref={formRef} initialData={{ name: 'Daniel' }} onSubmit={handleSubmit}>
                 <h1>Faça seu login</h1>
 
                 <Input name="name" icon={FiUser} placeholder="Nome" />
