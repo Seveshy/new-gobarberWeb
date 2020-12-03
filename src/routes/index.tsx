46c2a981027048aab104015c5d9ff9850b3d3f2e
Seveshy <danielalvesmajor@outlook.com>
2020-12-03 15:20:53 -0300

    Implementando redefinição de senha

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -6,6 +6,7 @@ import Route from './Route';
 import SingIn from '../pages/SingIn';
 import Signup from '../pages/Signup';
 import ForgotPassword from '../pages/ForgotPassword';
+import ResetPassword from '../pages/ResetPassword';
 
 import Dashboard from '../pages/Dashboard';
 
@@ -14,6 +15,7 @@ const Routes: React.FC = () => (
         <Route path="/" exact component={SingIn} />
         <Route path="/signup" component={Signup} />
         <Route path="/forgot-password" component={ForgotPassword} />
+        <Route path="/reset-password" component={ResetPassword} />
 
         <Route path="dashboard" component={Dashboard} isPrivate />
     </Switch>
