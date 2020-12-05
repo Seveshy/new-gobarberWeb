f1f341bab08ac503a996faaa6290d4a844d02982
Seveshy <danielalvesmajor@outlook.com>
2020-12-05 16:57:00 -0300

    Próximo agendamento

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,8 +1,18 @@
 import React from 'react';
 
-import { FiPower } from 'react-icons/fi';
+import { FiClock, FiPower } from 'react-icons/fi';
 
-import { Container, Header, HeaderContent, Profile } from './styles';
+import { 
+    Container, 
+    Header, 
+    HeaderContent, 
+    Profile,
+    Content,
+    Schedule,
+    Calendar,
+    NextAppointment
+
+} from './styles';
 
 import logoImg from '../../assets/logo.svg';
 import { useAuth } from '../../hooks/auth';
@@ -26,6 +36,31 @@ const Dashboard: React.FC = () => {
                 </button>
             </HeaderContent>
         </Header>
+        <Content>
+            <Schedule>
+                <h1>Horários agendados</h1>
+                <p>
+                    <span>Hoje</span>
+                    <span>Dia 06</span>
+                    <span>Segunda feira</span>
+                </p>
+                <NextAppointment>
+                    <strong>Atendimento a seguir</strong>
+                    <div>
+                        <img src="https://danielmajor.com.br/static/2a790485413811e589bbbb3b87753a35/245c4/daniel.jpg" alt="Daniel Major" />
+
+                        <strong>Daniel Major</strong>
+                        <span>
+                            <FiClock />
+                            08:00
+                        </span>
+                    </div>
+                </NextAppointment>
+                <Calendar>
+
+                </Calendar>
+            </Schedule>
+        </Content>
     </Container>
     )
 }
